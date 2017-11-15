d43b5f61f678bd6be040ab1faf302546f1d2b164
Alexander Shelega <alexander.shelega@gmail.com>
2017-11-15 12:36:48 +0400

    Update startupJenkinsJob.groovy

--- startupJenkinsJob.groovy
+++ startupJenkinsJob.groovy
@@ -7,7 +7,7 @@ multibranchPipelineJob('test_folder/test_job') {
         git {
             remote('https://github.com/alexandershelega/jenkins-sdp2.git')
             credentialsId('7df4f184-e3f6-4ff5-88a6-5087f907d57f ')
-            #include('master')
+            
         }
 	}
 
