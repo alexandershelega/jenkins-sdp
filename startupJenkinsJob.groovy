a886920ce87d1a9e78edbf248b316ccc5e5003d5
as <antimion@gmail.com>
2017-11-15 12:29:48 +0400

    ss

--- startupJenkinsJob.groovy
+++ startupJenkinsJob.groovy
@@ -1,18 +1,16 @@
 folder("test_folder") { }
 
 
-pipelineJob('test_folder/test_job') {
+multiBipelineJob('test_folder/test_job') {
    description('test_job')
-
-scm {
+	branchSources {
         git {
-		remote{
-			url('https://github.com/alexandershelega/jenkins-sdp2.git')
-            		credentialsId('7df4f184-e3f6-4ff5-88a6-5087f907d57f ')
-	    		include('master')
-		}
+            remote('https://github.com/alexandershelega/jenkins-sdp2.git')
+            credentialsId('7df4f184-e3f6-4ff5-88a6-5087f907d57f ')
+            include('master')
         }
-}
+	}
+
     orphanedItemStrategy {
         discardOldItems {
             daysToKeep(1)
