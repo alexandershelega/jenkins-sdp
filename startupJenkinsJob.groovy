5f9bd0231cd9e3f81101ef37a29b2ea61f8a2aaf
Alexander Shelega <alexander.shelega@gmail.com>
2017-11-15 13:55:02 +0400

    Update startupJenkinsJob.groovy

--- startupJenkinsJob.groovy
+++ startupJenkinsJob.groovy
@@ -24,22 +24,29 @@ folder("simple") { }
 pipelineJob('simple/simple_pipeline_job') {
 displayName('simple_pipeline_test')
 	definition {
-    	cpsScm {
-    		scm {
-    	    	git {
-    	        	remote {
-    	            	name('sdp2')
-    	            	url('https://github.com/alexandershelega/jenkins-sdp2.git')
-			credentials('a85736fd-a8d3-4686-9e6a-bc9fe9585767')
-    	            	branch('master')
-    	        	}
-    	    	}
-   	     	}
-    	}
+	    	cpsScm {
+	    		scm {
+	    	    	git {
+	    	        	remote {
+	    	            	name('sdp2')
+	    	            	url('https://github.com/alexandershelega/jenkins-sdp2.git')
+							credentials('a85736fd-a8d3-4686-9e6a-bc9fe9585767')
+	    	            	branch('master')
+	    	        	}
+	    	    	    }
+	   	     	}
+	    	    }
+		}
+	properties {
+		buildDiscarder {
+			strategy {
+				logRotator {
+					artifactDaysToKeepStr(1)
+					artifactNumToKeepStr(1)
+					daysToKeepStr(1)
+					numToKeepStr(1)
+				}
+			}
+		}
 	}
-    orphanedItemStrategy {
-        discardOldItems {
-            daysToKeep(7)
-        }
-    }	
 }
