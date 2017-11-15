d7d7ddb322b060098c93d7bd69b750a02ee0a0ca
Alexander Shelega <alexander.shelega@gmail.com>
2017-11-15 12:15:51 +0400

    Update startupJenkinsJob.groovy

--- startupJenkinsJob.groovy
+++ startupJenkinsJob.groovy
@@ -4,13 +4,11 @@ folder("test_folder") { }
 pipelineJob('test_folder/test_job') {
    description('test_job')
 
-branchSources {
         git {
             remote('https://github.com/alexandershelega/jenkins-sdp2.git')
             credentialsId('7df4f184-e3f6-4ff5-88a6-5087f907d57f ')
 	    include('master')
         }
-}
 
     orphanedItemStrategy {
         discardOldItems {
