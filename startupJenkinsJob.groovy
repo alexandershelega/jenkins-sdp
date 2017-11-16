7abb498b2007da31e04e62913c170e8d162eaa23
Alexander Shelega <alexander.shelega@gmail.com>
2017-11-16 14:15:13 +0400

    Update startupJenkinsJob.groovy

--- startupJenkinsJob.groovy
+++ startupJenkinsJob.groovy
@@ -60,7 +60,7 @@ description('analytics_node_images')
 	    		scm {
 	    	    	git {
 	    	        	remote {
-				name('master')
+				name('picsart-analytics')
 	    	            	url('https://github.com/PicsArt/picsart-analytics.git')
 				credentials('8f772c78-46fe-4f49-9599-3c3d4d32ae90')
 	    	            	branch('master')
